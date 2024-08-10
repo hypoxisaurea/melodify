25586b0cb49ca04ceda18d2be7574f829c437a8b
hypoxisaurea <flickerlight14@naver.com>
2024-08-10 23:51:49 +0900

    [Feat] Update Settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AnimatePresence } from 'framer-motion';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
+
+import Splash from './pages/Splash';
+import InputPage from './pages/InputPage';
+import OutputPage from './pages/OutputPage';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Splash />} />
+        <Route path="/select" element={<InputPage />} />
+        <Route path="/result" element={<OutputPage />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
